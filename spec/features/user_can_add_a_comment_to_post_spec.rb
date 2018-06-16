6201c3d07952b8a1f5bf47769c71128701858236
Asia Antczak <aasiaantczak@gmail.com>
2018-06-16 17:39:44 +0100

    Display name of the user who created the comment

--- spec/features/user_can_add_a_comment_to_post_spec.rb
+++ spec/features/user_can_add_a_comment_to_post_spec.rb
@@ -7,9 +7,23 @@ feature 'comment' do
     attach_file('post[image]', "spec/files/images/mum.jpeg")
     fill_in 'post[description]', with: 'me and my sweet kids'
     click_button 'submit'
-    puts page.body
     fill_in 'comment[content]', with: 'What a lovely pic! Miss you both!'
     click_button 'Comment'
     expect(page).to have_content 'What a lovely pic! Miss you both!'
+  end
+
+  scenario 'display name of the user who added a comment' do
+    sign_up_and_sign_in
+    click_link 'New Post'
+    attach_file('post[image]', "spec/files/images/mum.jpeg")
+    fill_in 'post[description]', with: 'me and my sweet kids'
+    click_button 'submit'
+    click_link 'Logout'
+    sign_up_and_sign_in_other_user
+    fill_in 'comment[content]', with: 'What a lovely pic! Miss you both!'
+    click_button 'Comment'
+    click_link 'Logout'
+    sign_in
+    expect(page).to have_content 'Lucy'
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -7,3 +7,21 @@ def sign_up_and_sign_in
   fill_in('Password confirmation', with: 'emma11', match: :prefer_exact)
   click_button "Sign up"
 end
+
+def sign_up_and_sign_in_other_user
+  visit "/"
+  click_link "Register"
+  fill_in "Name", with: "Lucy"
+  fill_in "Email", with: "lucy@hotmail.com"
+  fill_in('Password', with: 'lucy11', match: :first)
+  fill_in('Password confirmation', with: 'lucy11', match: :prefer_exact)
+  click_button "Sign up"
+end
+
+def sign_in
+  visit "/"
+  click_link "Login"
+  fill_in "Email", with: "emma@hotmail.com"
+  fill_in('Password', with: 'emma11', match: :first)
+  click_button 'Log in'
+end
